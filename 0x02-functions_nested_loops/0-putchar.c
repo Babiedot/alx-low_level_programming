c433f51a5c708c6f6f4a11926adf1c328896dffa
Babiedot <babiedot@gmail.com>
2022-12-14 05:17:30 +0100

    Trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
-main.h
+#include "main.h"
+/**
+ *main - Print "_putchar" followed by a new line.
+ *Description: You are not allowed to include standard libr
+ ariint
+ *Return: 0
+ */
+int main(void)
+{
+	char alx[] = "_putchar";
+	int i = 0;
+	while (alx[i] != '\0')
+	{
+		_putchar(alx[i]);
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+		i++;
+
+	}
+
+	_putchar('\n')
+}
 
